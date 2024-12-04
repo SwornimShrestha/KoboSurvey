13e34f122c1a1be803b340f127eff03963a5bf72
Swornim Shrestha <swornimshrestha639@gmail.com>
2024-12-04 16:22:43 +0545

    -fix error

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,6 @@ import { MantineProvider } from '@mantine/core';
 
 import '@mantine/core/styles.css';
 import '@mantine/dates/styles.css';
-import 'mantine-react-table/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
